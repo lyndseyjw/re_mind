f7e41e8bfe5c0c70b83aa5c34b24bc94832d2fa5
lyndseyjw <lyndseyjwatson@gmail.com>
2021-10-16 15:00:26 -0700

    commit to pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 
 function App() {
   return (

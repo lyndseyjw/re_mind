389e3b76a19b0dce8980c3a918db77e457df77a4
candiceywu <candice.ying.wu@gmail.com>
2021-10-20 12:44:44 -0700

    chart rendering

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -4,65 +4,89 @@ import {QUERY_WATER} from './../utils/queries';
 //function to create data, map through data
 //this is the chart on the right of dashboard that would render the water, social, etc. intake 
 
-const data = [{QUERY_WATER}]
-// [
-//   {
-//     day: 'Day 1',
-//     cups: 4,
+const data = 
+// [{QUERY_WATER}];
 
-//   },
-//   {
-//     day: 'Day 2',
-//     cups: 6,
-//   },
-//   {
-//     day: 'Day 3',
-//     cups: 4,
-//   },
-//   {
-//     day: 'Day 4',
-//     cups: 8,
-//   },
-//   {
-//     day: 'Day 5',
-//     cups: 6,
-//   },
-//   {
-//     day: 'Day 6',
-//     cups: 5,
-//   },
-//   {
-//     day: 'Day 7',
-//     cups: 8,
-//   },
-// ];
+[
+  {
+    name: 'Day 1',
+    uv: 4,
 
-export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/simple-area-chart-4ujxw';
+  },
+  {
+    name: 'Day 2',
+    uv: 6,
+  },
+  {
+    name: 'Day 3',
+    uv: 4,
+  },
+  {
+    name: 'Day 4',
+    uv: 8,
+  },
+  {
+    name: 'Day 5',
+    uv: 6,
+  },
+  {
+    name: 'Day 6',
+    uv: 5,
+  },
+  {
+    name: 'Day 7',
+    uv: 8,
+  },
+];
 
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <AreaChart width={730} height={250} data={data}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-          <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
-        </AreaChart>
-      </ResponsiveContainer>
-    );
-  }
+export default function Chart() {
+  return (
+    <AreaChart
+      width={500}
+      height={400}
+      data={data}
+      margin={{
+        top: 10,
+        right: 30,
+        left: 0,
+        bottom: 0
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+    </AreaChart>
+  );
 }
+
+// export default class Chart extends PureComponent {
+//   static demoUrl = 'https://codesandbox.io/s/simple-area-chart-4ujxw';
+
+//   render() {
+//     return (
+//       <ResponsiveContainer width="100%" height="100%">
+//         <AreaChart width={730} height={250} data={data}
+//           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+//           <defs>
+//             <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+//               <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+//               <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+//             </linearGradient>
+//             <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+//               <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+//               <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+//             </linearGradient>
+//           </defs>
+//           <XAxis dataKey="name" />
+//           <YAxis />
+//           <CartesianGrid strokeDasharray="3 3" />
+//           <Tooltip />
+//           <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+//           <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+//         </AreaChart>
+//       </ResponsiveContainer>
+//     );
+//   }
+// }
---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Water from './Water';
+import Social from './Social';
+import Outside from './Outside';
+import Auth from '../utils/auth';

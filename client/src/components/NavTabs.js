265c61909ce3466c08ca164e2529d871f751933e
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-10-20 10:17:21 -0700

    navbar update + sleep and mood update

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -1,51 +1,48 @@
 import React from "react";
 import "../App.css";
+import {Nav , NavDropdown} from 'react-bootstrap'
 
 function NavTabs({ currentPage, handlePageChange }) {
+  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+
   return (
-    <nav
-      className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
-      id="sideNav"
-    >
-      <div className="collapse navbar-collapse" id="navbarResponsive">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a
-              href="#dashboard"
-              onClick={() => handlePageChange("Dashboard")}
-              className={
-                currentPage === "dashboard" ? "nav-link active" : "nav-link"
-              }
-            >
-              Dashboard
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#upload"
+ 
+ <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
+    <NavDropdown title="Dropdown" id="nav-dropdown">
+        <NavDropdown.Item eventKey="4.1">Sleep</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.2">Gratitude</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.3">Mood</NavDropdown.Item>
+        <NavDropdown.Divider />
+      </NavDropdown>
+      <Nav.Item>
+        <Nav.Link eventKey="1" href="#dashboard" onClick={() => handlePageChange("Dashboard")}
+              className={currentPage === "dashboard" ? "nav-link active" : "nav-link"} >
+          Dashboard
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="2" title="Item" href="#upload"
               onClick={() => handlePageChange("Upload")}
               className={
-                currentPage === "upload" ? "nav-link active" : "nav-link"
-              }
-            >
+                currentPage === "upload" ? "nav-link active" : "nav-link" 
+              } >
               Upload
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#logout"
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="3" disabled href="#logout"
               onClick={() => handlePageChange("Logout")}
               className={
                 currentPage === "logout" ? "nav-link active" : "nav-link"
               }
             >
-              Logout
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          Logout
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+
   );
 }
+
 
 export default NavTabs;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
 
     return (
         <main>
+            
             <div className="flex-row justify-center">
                 <div
                     className="col-12 col-md-10 mb-3 p-3"
